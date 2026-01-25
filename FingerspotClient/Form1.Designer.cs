7de335847fdc48a110fc515085b6bc998d12c607
Rheinard <christiansadeko95@gmail.com>
2026-01-25 08:40:06 +0700

    Fix structure: Move .sln to root and re-init git

---
+++ FingerspotClient/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace FingerspotClient
+{
+    partial class FingerspotClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelSidebar = new System.Windows.Forms.Panel();
+            this.PanelHeader = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ButtonMin = new System.Windows.Forms.Button();
+            this.ButtonMax = new System.Windows.Forms.Button();
+            this.ButtonExit = new System.Windows.Forms.Button();
+            this.PanelMain = new System.Windows.Forms.Panel();
+            this.PanelHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PanelSidebar
+            // 
+            this.PanelSidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.PanelSidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelSidebar.Location = new System.Drawing.Point(0, 39);
+            this.PanelSidebar.Name = "PanelSidebar";
+            this.PanelSidebar.Size = new System.Drawing.Size(246, 561);
+            this.PanelSidebar.TabIndex = 0;
+            // 
+            // PanelHeader
+            // 
+            this.PanelHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.PanelHeader.Controls.Add(this.label1);
+            this.PanelHeader.Controls.Add(this.pictureBox1);
+            this.PanelHeader.Controls.Add(this.ButtonMin);
+            this.PanelHeader.Controls.Add(this.ButtonMax);
+            this.PanelHeader.Controls.Add(this.ButtonExit);
+            this.PanelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelHeader.Location = new System.Drawing.Point(0, 0);
+            this.PanelHeader.Name = "PanelHeader";
+            this.PanelHeader.Size = new System.Drawing.Size(1200, 39);
+            this.PanelHeader.TabIndex = 1;
+            this.PanelHeader.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelHeader_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.label1.Location = new System.Drawing.Point(39, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Fingerspot Client";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelHeader_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Image = global::FingerspotClient.Properties.Resources.icon_64x64;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(39, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelHeader_MouseDown);
+            // 
+            // ButtonMin
+            // 
+            this.ButtonMin.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ButtonMin.FlatAppearance.BorderSize = 0;
+            this.ButtonMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonMin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.ButtonMin.Location = new System.Drawing.Point(1062, 0);
+            this.ButtonMin.Name = "ButtonMin";
+            this.ButtonMin.Size = new System.Drawing.Size(46, 39);
+            this.ButtonMin.TabIndex = 2;
+            this.ButtonMin.Text = "➖";
+            this.ButtonMin.UseVisualStyleBackColor = true;
+            this.ButtonMin.Click += new System.EventHandler(this.ButtonMin_Click);
+            // 
+            // ButtonMax
+            // 
+            this.ButtonMax.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ButtonMax.FlatAppearance.BorderSize = 0;
+            this.ButtonMax.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonMax.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.ButtonMax.Location = new System.Drawing.Point(1108, 0);
+            this.ButtonMax.Name = "ButtonMax";
+            this.ButtonMax.Size = new System.Drawing.Size(46, 39);
+            this.ButtonMax.TabIndex = 1;
+            this.ButtonMax.Text = "▢";
+            this.ButtonMax.UseVisualStyleBackColor = true;
+            this.ButtonMax.Click += new System.EventHandler(this.ButtonMax_Click);
+            // 
+            // ButtonExit
+            // 
+            this.ButtonExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ButtonExit.FlatAppearance.BorderSize = 0;
+            this.ButtonExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            this.ButtonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.ButtonExit.Location = new System.Drawing.Point(1154, 0);
+            this.ButtonExit.Name = "ButtonExit";
+            this.ButtonExit.Size = new System.Drawing.Size(46, 39);
+            this.ButtonExit.TabIndex = 0;
+            this.ButtonExit.Text = "🗙";
+            this.ButtonExit.UseVisualStyleBackColor = true;
+            this.ButtonExit.Click += new System.EventHandler(this.ButtonExit_Click);
+            // 
+            // PanelMain
+            // 
+            this.PanelMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.PanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelMain.Location = new System.Drawing.Point(246, 39);
+            this.PanelMain.Name = "PanelMain";
+            this.PanelMain.Size = new System.Drawing.Size(954, 561);
+            this.PanelMain.TabIndex = 2;
+            // 
+            // FingerspotClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1200, 600);
+            this.Controls.Add(this.PanelMain);
+            this.Controls.Add(this.PanelSidebar);
+            this.Controls.Add(this.PanelHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FingerspotClient";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FingerspotClient";
+            this.PanelHeader.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelSidebar;
+        private System.Windows.Forms.Panel PanelHeader;
+        private System.Windows.Forms.Panel PanelMain;
+        private System.Windows.Forms.Button ButtonExit;
+        private System.Windows.Forms.Button ButtonMin;
+        private System.Windows.Forms.Button ButtonMax;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ FingerspotClient/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace FingerspotClient
+{
+    public partial class FingerspotClient : Form
+    {
+        public FingerspotClient()
+        {
+            InitializeComponent();
+
+            // Mencegah Form menutupi Taskbar saat WindowState = Maximized
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+        }
+
+        // Memungkinkan drag form tanpa border
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void panelHeader_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Memanggil fungsi Windows untuk memulai drag window
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void ButtonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ButtonMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void ButtonMax_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                // Jika sedang ukuran normal, maka diperbesar
+                this.WindowState = FormWindowState.Maximized;
+                ButtonMax.Text = "❐";
+            }
+            else
+            {
+                // Jika sedang diperbesar, maka dikembalikan ke ukuran normal
+                this.WindowState = FormWindowState.Normal;
+                ButtonMax.Text = "▢";
+            }
+        }
+    }
+}
